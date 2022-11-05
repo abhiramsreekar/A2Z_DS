96170c22ce935b705e4d56dd048440a767d57e66
Abhiram Sreekar <83418471+abhiramsreekar@users.noreply.github.com>
2022-11-05 23:11:30 +0530

    Delete Basic Recursion directory

--- Basic Recursion/printNTimes.cpp
+++
@@ -1,9 +0,0 @@
-class Solution{
-    public:
-    //Complete this function
-    void printNos(int N)
-    {
-        if(N>1) printNos(N-1);
-        cout<<N<<" ";
-    }
-};
--- Basic Recursion/printNameNTimes.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-  public:
-    void printGfg(int N) {
-        // Code here
-        if(N>1) printGfg(N-1);
-        cout<<"GFG ";
-    }
-};
