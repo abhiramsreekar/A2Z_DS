af3b8914f1f6e088814a38e3ab5342c975e41fda
Abhiram Sreekar <83418471+abhiramsreekar@users.noreply.github.com>
2022-11-07 00:34:42 +0530

    Create removeDup.cpp

---
+++ Arrays/Easy/removeDup.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+    int remove_duplicate(int arr[],int n){
+        int i=0;
+        for(int j=1;j<n;j++)
+        {
+            if(arr[i]!=arr[j])
+            {
+                arr[++i]=arr[j];
+            }
+        }
+        return i+1;
+    }
+};
