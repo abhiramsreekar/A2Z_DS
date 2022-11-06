ea42360c7640e62208225bf096b3f5c3b8c7d896
Abhiram Sreekar <83418471+abhiramsreekar@users.noreply.github.com>
2022-11-07 00:19:59 +0530

    Create checkArraySorted.cpp

---
+++ Arrays/Easy/checkArraySorted.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i]>arr[i+1]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
