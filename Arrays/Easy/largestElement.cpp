38b2ec57d84ad0a795b2582c73aa18e36a044efa
Abhiram Sreekar <83418471+abhiramsreekar@users.noreply.github.com>
2022-11-07 00:02:21 +0530

    Create largestElement.cpp

---
+++ Arrays/Easy/largestElement.cpp
@@ -0,0 +1,26 @@
+//Using the ma_element function of vector
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        return *max_element(arr.begin(),arr.end());
+    }
+};
+
+//Approach without using in-built methods
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        int m=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            if(m<arr[i]) m=arr[i];
+        }
+        return m;
+    }
+};
